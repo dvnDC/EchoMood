3651bda30bf552ecf62b6d8f1ae13352b94d5d8f
Damian Cichosz <damcichosz@gmail.com>
2025-04-06 01:03:19 +0200

    Update references and validations for MoodEntry and User

--- app/models/mood_entry.rb
+++ app/models/mood_entry.rb
@@ -1,3 +1,6 @@
 class MoodEntry < ApplicationRecord
   belongs_to :user
+
+  validates :mood_level, presence: true, inclusion: { in: 1..5 }
+  validates :entry_date, presence: true, uniqueness: { scope: :user_id } # One mood entry per day per user.
 end
