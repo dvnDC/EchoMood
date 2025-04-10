a372f4cfccc793225bf5207e155a02b653725993
Damian Cichosz <damcichosz@gmail.com>
2025-04-11 00:58:51 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_05_223911) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_08_230340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "emotion_entries", force: :cascade do |t|
+    t.integer "valence", null: false
+    t.integer "arousal", null: false
+    t.text "notes"
+    t.bigint "user_id", null: false
+    t.date "date", null: false
+    t.datetime "entry_time", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "date", "entry_time"], name: "index_emotion_entries_on_user_id_and_date_and_entry_time"
+    t.index ["user_id"], name: "index_emotion_entries_on_user_id"
+  end
 
   create_table "mood_entries", force: :cascade do |t|
     t.integer "mood_level"
@@ -36,5 +49,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_05_223911) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "emotion_entries", "users"
   add_foreign_key "mood_entries", "users"
 end
