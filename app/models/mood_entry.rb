a9598ad3479b85c3d704e92c89eff0816ba9f5fc
Damian Cichosz <damcichosz@gmail.com>
2025-04-13 01:47:02 +0200

    Add note for mood_level for MoodEntry model

--- app/models/mood_entry.rb
+++ app/models/mood_entry.rb
@@ -1,7 +1,7 @@
 class MoodEntry < ApplicationRecord
   belongs_to :user
 
-  validates :mood_level, presence: true, inclusion: { in: 1..5 }
+  validates :mood_level, presence: true, inclusion: { in: 1..5 } # Update articles with this data - meditation and patterns!
   validates :entry_date, presence: true
 
   before_validation :set_default_entry_date, on: :create
