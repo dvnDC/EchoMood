b1e09f336cc0a852c4bc82a98c7598aa185225b8
Damian Cichosz <damcichosz@gmail.com>
2025-04-15 03:20:44 +0200

    Add roles and seed default admin user
    This commit introduces seeding logic for roles ('admin', 'regular', 'cool') and ensures an admin user with a default email and password is created. The changes make the seed script more functional and suitable for initial setup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+roles = ['admin', 'regular', 'cool']
+
+roles.each do |role_name|
+  Role.find_or_create_by(name: role_name)
+end
+
+# Dodaj admina
+admin = User.find_or_create_by(email: 'admin@example.com') do |user|
+  user.password = 'password123'
+  user.password_confirmation = 'password123'
+end
+
+admin.add_role('admin')
