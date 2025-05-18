3f1aabbd08a18f62a8d1f6d3da2dcb708fbe8956
Damian Cichosz <damcichosz@gmail.com>
2025-05-18 05:07:05 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_17_022815) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_18_022815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -64,7 +64,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_17_022815) do
     t.string "name"
     t.text "bio"
     t.string "nickname"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email"
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
