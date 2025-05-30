fa4b4468cd729c843feac1961e9e483d9eaa474e
Damian Cichosz <damcichosz@gmail.com>
2025-05-30 19:04:44 +0200

    Add 'research_participant' role to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
-roles = ['admin', 'regular', 'cool']
+roles = ['admin', 'regular', 'cool', 'research_participant']
 
 roles.each do |role_name|
   Role.find_or_create_by(name: role_name)
 end
 
-# Dodaj admina
 admin = User.find_or_create_by(nickname: 'admin') do |user|
   user.password = 'password123'
   user.password_confirmation = 'password123'
